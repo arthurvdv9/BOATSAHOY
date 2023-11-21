310d31d3b6b7b5e1e4880542f1ccb6732f301bae
joana faria <paisdefaria.joana@gmail.com>
2023-11-21 16:39:00 +0000

    acept feature finally working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:show]
+  before_action :set_booking, only: [:show, ]
 
   def index
     @bookings = Booking.all
@@ -16,23 +16,13 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.boat = Boat.find(params[:boat_id])
     @booking.user = current_user
+
     if @booking.save
       redirect_to boats_path notice:"Booking was successfully created."
     else
       render :index, status: :unprocessable_entity
     end
   end
-
-private
-
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
-
-  def boat_params
-    params.require(:booking).permit(:start_date, :end_date, :total_price, :user_id, :boat_id)
-  end
-
 
   def accept
     @booking = Booking.find(params[:id])
@@ -46,6 +36,16 @@ private
     @booking.status = "rejected"
     @booking.save
     redirect_to bookings_path
+  end
+
+private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :boat_id)
   end
 
 
