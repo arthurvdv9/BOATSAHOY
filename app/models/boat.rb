a354067ca00077e914a936a823cbb451ca5826a5
joana faria <paisdefaria.joana@gmail.com>
2023-11-21 18:03:09 +0000

    starting validations

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,9 @@
 class Boat < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
+
+  validates :location, :name, presence: true
+  validates :capacity, presence: true, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { only_float: true }
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   validates :status, inclusion: { in: %w(accepted pending rejected) }
   belongs_to :user
   belongs_to :boat
+
+
 end
