4ba09f5592ceb0821dfa75bda7cf59e32eb191e1
Vasco Pereira <happen1337.ltd@gmail.com>
2023-11-22 14:29:25 +0000

    done

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -5,8 +5,13 @@ class BoatsController < ApplicationController
 
   def index
     @boats = Boat.all
+    @markers = @boats.geocoded.map do |boat|
+      {
+        lat: boat.latitude,
+        lng: boat.longitude
+      }
   end
-
+end
   def show
   end
 
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,7 +1,8 @@
 class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   validates :location, :name, presence: true
   validates :capacity, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: { only_float: true }
