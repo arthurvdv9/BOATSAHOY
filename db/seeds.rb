24e3299c6cd7bcd99e0b9fec74f62c7cc62effc7
joana faria <paisdefaria.joana@gmail.com>
2023-11-22 16:36:37 +0000

    latest changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ user = User.create!(email:"fsfa@gmail.com",  password: "123456")
 puts "User Created"
 
 puts "Creating Boat"
-file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+file = URI.open("https://images.unsplash.com/photo-1605281317010-fe5ffe798166?q=80&w=2044&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 boat = Boat.new(location:"Atlantis", name: "Ariel", capacity: 5, price: 300, user_id: user.id)
 boat.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 boat.save
