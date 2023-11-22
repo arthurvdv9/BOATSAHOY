02d66a6fe238a803f8f07d5f22c754469ff9a01a
Vasco Pereira <happen1337.ltd@gmail.com>
2023-11-22 15:07:52 +0000

    done

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,5 +14,21 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
+    this.#addMarkersToMap()
+
+  }
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      console.log(marker);
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
