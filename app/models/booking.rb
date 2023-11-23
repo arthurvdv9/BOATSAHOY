2a69866f9d1082232d6a94b4c08a049510d4990c
Arthur Van de Venne <arthur.vandevenne@gmail.com>
2023-11-23 15:20:44 +0000

    priiice

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :boat
 
+  def total_price
+    ActiveSupport::NumberHelper.number_to_currency(((end_date - start_date).to_i * boat.price), unit: "€", precision: 2)
+  end
+
 
 end
